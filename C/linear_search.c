755d560340d512906ba05f4bb298e303248dc22b
Dharan Aditya <dharan.aditya@gmail.com>
2017-08-13 23:31:25 +0530

    Update README.md & Linear Search description

--- C/linear_search.c
+++ C/linear_search.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+
 // Linear Search
-// This algorithm searches for the key in the given array in linear fashion i.e it compares every element with key
-// if key is found then it returns the index of key else it returns -1
+// This algorithm searches for a element x in a array in linear fashion.
+// It compares every element of the array with x
+// if true i.e array contains element x so it returns the index of x in the array
+// else it returns -1
+#include <stdio.h>
+// Linear Search implementation
 int linearSearch(int arr[],int size,int key){
   for (size_t i = 0; i < size; i++) {
     if(arr[i] == key)
@@ -11,7 +15,7 @@ int linearSearch(int arr[],int size,int key){
   }
   return -1;
 }
-
+// Main method to execute the program / Driver method
 int main(int argc, char const *argv[]) {
   int arr[] = {12,23,45,56,78,89,14,25,36,47,58,69};
   int size = sizeof(arr)/sizeof(arr[0]);
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Data Structures and Algorithms
+
+Implementation of Data Structures and Algorithms in C, Java and Python.
+
+## License
+[MIT](./LICENSE)
