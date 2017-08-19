b7e45b6e0368e9b7ea000d7f10c6bcb2bacde119
MvsKiran <kiranmvs07@gmail.com>
2017-08-20 00:07:12 +0530

    Added Algo's in java

--- Java/BinarySearch.java
+++ Java/BinarySearch.java
@@ -1,32 +1,38 @@
 // Java implementation of recursive Binary Search
+/*
+   The  BinarySearch--class provides a method for binary
+   searching for an integer in a sorted array of integers.
+*/
 class BinarySearch
-{
+    {
     // Returns index of x if it is present in arr[l..r], else
     // return -1
     int binarySearch(int arr[], int left, int right, int key)
     {
-      if(left <= right){
-    int mid = (left+right)/2;
-    if(arr[mid] == key)
+      if(left <= right)         //this is line tells us keep going until left is lowerthan or equalto right
     {
-      return mid;
+    int mid = (left+right)/2;   //ine 10: mid- we calculate so we can divide the list into two pieces.
+     
+	 if(arr[mid] == key)    //if the mid is equal to our key ,return mid
+    {
+     		 return mid;
     }
     if(key > mid)
     {
-      return binarySearch(arr,mid+1,right,key);
+      		return binarySearch(arr,mid+1,right,key); //if the key is greater than mid it search in right
     }
     else
     {
-      return binarySearch(arr,left,mid-1,key);
+      		return binarySearch(arr,left,mid-1,key);  //if the key is less than mid it search in left
     }
   }
   return -1;
 }
-  public static void main(String args[])
+public static void main(String args[])
     {
         BinarySearch ob = new BinarySearch();
         int arr[] = {2,3,4,10,40};
-        int n = arr.length;
+        int n = arr.length; 		//Array.length will give size of array
         int key = 10;
         int result = ob.binarySearch(arr,0,n-1,key);
         if (result == -1)
--- Java/LinearSearch.java
+++ Java/LinearSearch.java
@@ -1,25 +1,32 @@
-
+/*
+Linear Search
+This algorithm searches for a element let say- x in a array in linear manner.
+It compares every element of the array with x using for loop
+if array contains element x ,then it returns the index of x in the array,if x is not present in the array
+it returns -1
+*/
  
  class LinearSearch {
  
-    public static int linerSearch(int[] arr, int key){
-         
-        int size = arr.length;
-        for(int i=0;i<size;i++){
+    public static int linerSearch(int[] arr, int key) //passing two parametres(function)
+{
+    int size = arr.length;
+        for(int i=0;i<size;i++)  //Using loop to check whether the entered number is found in the list or not
+	{
             if(arr[i] == key){
                 return i;
-            }
+           }
         }
         return -1;
     }
      
     public static void main(String a[]){
          
-        int[] arr1= {23,45,21,55,234,1,34,90};
-        int searchKey = 34;
+        int[] arr1= {23,45,21,55,234,1,66,90};
+        int searchKey = 66;
         System.out.println("Key "+searchKey+" found at index: "+linerSearch(arr1, searchKey));
-        int[] arr2= {123,445,421,595,2134,41,304,190};
-        searchKey = 421;
+        int[] arr2= {123,445,24,595,2134,41,304,190};
+        searchKey = 24;
         System.out.println("Key "+searchKey+" found at index: "+linerSearch(arr2, searchKey));
     }
 }
