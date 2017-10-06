6bed850ee8baf392a6f8bf0e60ea3744e7fedc3e
Sturov Fedor <sturov.fedor@yandex.ru>
2017-10-06 13:42:32 +0300

    Update LinearSearch.kt

--- Kotlin/LinearSearch.kt
+++ Kotlin/LinearSearch.kt
@@ -1,13 +1,13 @@
 //Kotlin has indexOf for linear search
 fun main(args: Array<String>) {
     val haystack = listOf("Zig", "Zag", "Wally", "Ronald", "Bush", "Krusty", "Charlie", "Bush", "Boz", "Zag")
-    println(haystack)
-    var needle = "Zag"
-    var index  = haystack.indexOf(needle)
-    val index2 = haystack.lastIndexOf(needle)
-    println("\n'$needle' first occurs at index $index of the list")
-    println("'$needle' last  occurs at index $index2 of the list\n")
-    needle = "Donald"
-    index  = haystack.indexOf(needle)
-    if (index == -1) throw Exception("$needle does not occur in the list")
+    val needles = listOf("Zig", "Krusty", "Washington")
+    for (needle in needles){
+          val index = haystack.indexOf(needle)
+          if (index != -1) {
+            print(index.toString(10) + " is the index of " + needle + "\n")
+          } else {
+            print(needle + " is not in haystack\n")
+          }
+    }
 }
