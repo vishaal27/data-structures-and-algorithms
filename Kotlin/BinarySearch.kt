b1d726b385db47e5a814a55bba5a52982bc0af45
Sturov Fedor <sturov.fedor@yandex.ru>
2017-10-06 13:02:13 +0300

    Update BinarySearch.kt

--- Kotlin/BinarySearch.kt
+++ Kotlin/BinarySearch.kt
@@ -1,3 +1,19 @@
+// Kotlin implementation of recursive Binary Search
+/*
+Given the starting point of a range, the ending point of a range, and the "secret value", implement a binary search through a sorted integer array for a certain number. Implementations can be recursive or iterative (both if you can). Print out whether or not the number was in the array afterwards. If it was, print the index also.
+
+There are several binary search algorithms commonly seen. They differ by how they treat multiple values equal to the given value, and whether they indicate whether the element was found or not. For completeness we will present pseudocode for all of them.
+
+All of the following code examples use an "inclusive" upper bound (i.e. high = N-1 initially). Any of the examples can be converted into an equivalent example using "exclusive" upper bound (i.e. high = N initially) by making the following simple changes (which simply increase high by 1):
+
+change high = N-1 to high = N
+change high = mid-1 to high = mid
+(for recursive algorithm) change if (high < low) to if (high <= low)
+(for iterative algorithm) change while (low <= high) to while (low < high)
+
+NB: We add binary search capabilities to arrays.
+
+*/
 fun <T : Comparable<T>> Array<T>.iterativeBinarySearch(target: T): Int {
     var hi = size - 1
     var lo = 0
