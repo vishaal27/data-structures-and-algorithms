c2eb78f9b9209971c26f7e489cf1afcc8e50aa42
Vishaal Udandarao <vishaal16119@iiitd.ac.in>
2017-10-07 13:14:21 +0530

    Update AdamNumber.java

--- Java/AdamNumber.java
+++ Java/AdamNumber.java
@@ -1,4 +1,4 @@
-
+//Adam number is a number whose square is the reverse of the square of the number's reverse.
 
 import java.util.*;
 import java.lang.*;
@@ -9,19 +9,24 @@ public class AdamNumber
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		int n=12;
+		
+		//Prints if the number 12 is an Adam number or not.
 		System.out.println(isAdam(12));
 	}
 	
+	//Function to check if the number is an Adam number or not.
 	public static boolean isAdam(int n)
 	{
 		return (square(n)==returnReverse(square(returnReverse(n))));
 	}
 	
+	//function to calculate square of a number.
 	public static int square(int n)
 	{
 		return n*n;
 	}
 	
+	//function to find the reverse of a number.
 	public static int returnReverse(int n)
 	{
 		int rev=0;
